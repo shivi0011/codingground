bb0e6f2c3d237afebe0e3282c1c227e2ce5d0a6a
shivi0011 <shivi_patel@hotmail.com>
2017-06-20 16:51:10 +0530

    Update copyContentsOfOneFileToOther.c

--- C Prog/File Handling/copyContentsOfOneFileToOther.c
+++ C Prog/File Handling/copyContentsOfOneFileToOther.c
@@ -9,15 +9,19 @@ int main(void)
    if(fp1==NULL) printf("Error in opening the file!!\n");
    else
    {
-       fp2 = fopen("newTest.txt","w");
+       fp2 = fopen("newTest.txt","a");
        for(ch=fgetc(fp1);ch!=EOF;ch=fgetc(fp1))
         {
              fputc(ch,fp2);
         }
+       
+        fclose(fp2);
+        fp2 = fopen("newTest.txt","r");
+   
        for(ch=fgetc(fp2);ch!=EOF;ch=fgetc(fp2))
         {
-             //printf("%c",ch);
-             putchar(ch);
+             //printf("%c",ch);        //both are same
+              putchar(ch);             //prints single char only
         }
        
        fclose(fp1);
