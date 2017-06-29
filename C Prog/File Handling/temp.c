f648e70ce691b479231159dc4f9ec16782ad5219
shivi0011 <shivi_patel@hotmail.com>
2017-06-29 17:31:43 +0530

    Update temp.c

--- C Prog/File Handling/temp.c
+++ C Prog/File Handling/temp.c
@@ -143,7 +143,7 @@ int main (int argc , char * argv [] ){
            buffer[i] = ~buffer[i];
            //printf("%02X ",((unsigned char)buffer[i]));
        }
-       fseek(file, 0, SEEK_END);
+       fseek(file, 0, SEEK_SET);
        //fwrite(buffer,fileLen,sizeof(unsigned char),file);
        
        for(i=0;i<fileLen;i++)
@@ -164,6 +164,76 @@ int main (int argc , char * argv [] ){
 
         return 0;
 }
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (int argc , char * argv [] ){
+
+        // argv[1] = 1.jpg
+
+        FILE *file;
+        FILE *fp1;
+        unsigned char *buffer;
+        unsigned long fileLen;
+
+        //Open file
+        file = fopen(argv[1], "rb");
+        if (!file)
+        {
+                fprintf(stderr, "Unable to open file %s", argv[1]);
+                return;
+        }
+        fp1 = fopen("temp.png","ab+");
+        if(fp1==NULL){printf("Error in opening the file!");return;}
+        //Get file length
+        fseek(file, 0, SEEK_END);
+        fileLen=ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        //Allocate memory
+        buffer=(char *)malloc(fileLen);
+        if (!buffer)
+        {
+                fprintf(stderr, "Memory error!");
+                                fclose(file);
+                return 1;
+        }
+       
+       //fread(buffer,fileLen,sizeof(unsigned char),file);
+       //printf("\n3. Current position is %d\n",ftell(file));
+       int i;
+       for(i=0;i<fileLen;i++)
+       {
+           buffer[i] = fgetc(file);
+           //printf("%02X ",((unsigned char)buffer[i]));
+           buffer[i] = ~buffer[i];
+           //printf("%02X ",((unsigned char)buffer[i]));
+       }
+       fseek(file, 0, SEEK_SET);
+       //fwrite(buffer,fileLen,sizeof(unsigned char),file);
+       
+       for(i=0;i<fileLen;i++)
+       {
+       fputc(buffer[i],fp1);
+       }
+       fclose(file);
+       fclose(fp1);
+       i=0;
+
+       while (i < fileLen){
+           //printf("%02X ",((unsigned char)buffer[i]));
+           char chh = (unsigned char)buffer[i];
+           printf("%d ",chh);
+           i++;
+           if( ! (i % 16) ) printf( "\n" );
+       }
+
+        return 0;
+}
+
 
 http://www.linuxquestions.org/questions/programming-9/c-howto-read-binary-file-into-buffer-172985/
 https://stackoverflow.com/questions/39823509/inverting-colors-of-pixels-in-a-bmp-file-in-c
