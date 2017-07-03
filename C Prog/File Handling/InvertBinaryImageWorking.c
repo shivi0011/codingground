f4a05863024d43e9b86ac62a62547a36b9c572fc
shivi0011 <shivi_patel@hotmail.com>
2017-07-03 16:28:29 +0530

    Update InvertBinaryImageWorking.c

--- C Prog/File Handling/InvertBinaryImageWorking.c
+++ C Prog/File Handling/InvertBinaryImageWorking.c
@@ -1,6 +1,7 @@
 //this also not inverting the vertical pixels as they are not belonging to completely to any pixel
 //every image file (ex-BMP) has a header and trailer embeded with it so while inverting the image we need not 
 //to do anything to those pixels
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -10,7 +11,7 @@ int main ()
         FILE *fp2;
      
         fp1 = fopen("bb.bmp", "rb");
-        if (!fp1)
+        if (fp1==NULL)
         {
                 printf("Error in opening the file!!");
                 return 0;
@@ -33,18 +34,19 @@ int main ()
 
                  if(!feof(fp1))
                  {
-                    if(i>172)
+                    if(i>170)
                     {
                          if(a == 0)
                          {
                             printf("%u",a);
 
-                            unsigned char g = 0xff;
+                            //unsigned char g = 0xff;    also working but it wont work for all in case of other colors
+                            unsigned char g = 0xff-a;
                             fputc(g,fp2);
                             continue;
                          }
 
-                         fputc(0,fp2);        //value should be in hexadecimal... 0 is same as 0x00
+                         fputc(0xff-a,fp2);        //value should be in hexadecimal... 0 is same as 0x00
                          continue;
                     }
                  fputc(a,fp2);            //this will be the header and trailer of BMP file which should
