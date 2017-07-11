a32ef6c09ca21ad36d4f9d3ae6bb23d014dcd611
shivi0011 <shivi_patel@hotmail.com>
2017-07-11 12:31:26 +0530

    Update convolve.c

--- C Prog/File Handling/Convolution/convolve.c
+++ C Prog/File Handling/Convolution/convolve.c
@@ -38,47 +38,26 @@ int main()
         
         while(fread(&buff,fileLen,1,fp1)==1)
         {
-            //printf("%u ",ch);
-            //unsigned char temp = 0xff-ch;
-            fwrite(&buff,fileLen,1,fp2);
+            //fwrite(&buff,fileLen,1,fp1);
         }
+        
+        printf("%d\n",sizeof(buff));
         
         for(i=1;i<r-1;i++)
         {
             for(j=1;j<c-1;j++)
             {
                 int sum=0;
-                /*copy 3x3 into tmp array*/
-                x=0;
                 for(k=0;k<3;k++)
                 {
-                    y=0;
                     for(l=0;l<3;l++)
                     {
-                        sum += kernel[k][l] * buff[i-1+x][j-1+y];   
-                        
-                        y++;
+                        sum += kernel[k][l] * buff[i-1+k][j-1+l]; 
                     }
-                    x++;
                 }
                 buff[i][j] = sum;
             }
         }
-        
-        
-        
-        unsigned char ch;int max=0;
-        while(fread(&ch,1,1,fp1)==1)
-        {
-            printf("%u ",ch);
-            
-            if(ch>max) max = ch;
-            
-            unsigned char temp = 0xff-ch;
-            fwrite(&temp,1,1,fp2);
-           
-        }
-        printf("\nMax is %d\n",max);
         
         fclose(fp1);
         fclose(fp2);
