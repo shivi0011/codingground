d80b55624b4ff315d3c1a3f5f0713e32f6cab4c0
shivi0011 <shivi_patel@hotmail.com>
2017-07-21 11:58:42 +0530

    Create pointerToDerivedType.cpp

---
+++ C- Prog Practice/pointerToDerivedType.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+class base
+{
+    int i;      //by default all the member variables are private and can be accessed by only member fns
+    public:
+    void set_i(int n){i=n;}
+    int get_i(){return i;}
+};
+
+class derived:public base
+{
+    int j;
+    public:
+    void set_j(int n){j=n;}
+    int get_j(){return j;}
+    
+};
+
+int main()
+{
+    base *bp;
+    derived d;
+    
+    bp=&d;  //base pointer points to derived obj
+    
+    //access derived object using base pointer
+    bp->set_i(10);                      //need to use arrow operator to access thru pointer
+    cout<<bp->get_i()<<endl;
+
+/* This wont work. You cant access an element of a derived class using a base class pointer
+    bp->set_j(40);    
+    cout<<get_j()<<endl;
+*/
+    return 0;
+}
